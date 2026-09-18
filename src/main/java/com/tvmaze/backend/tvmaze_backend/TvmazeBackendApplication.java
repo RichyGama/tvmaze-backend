43e1a94f0144de17d0cbe8d8eaf0cb484cee0134
Ricardo Gama <gamaramosr@gmail.com>
2026-09-18 09:21:00 -0600

    initial project tvmaze

---
+++ src/main/java/com/tvmaze/backend/tvmaze_backend/TvmazeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tvmaze.backend.tvmaze_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvmazeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvmazeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tvmaze/backend/tvmaze_backend/TvmazeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvmaze.backend.tvmaze_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvmazeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
